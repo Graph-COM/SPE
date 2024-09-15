fix the missing mlp normalization of zinc training

src/data_utils/batch.py
@@ -25,7 +25,7 @@ class Batch(Data):
         Additionally, creates assignment batch vectors for each key in
         :obj:`follow_batch`."""
 
-        keys = [set(data.keys) for data in data_list]
+        keys = [set(data.keys()) for data in data_list]
         keys = list(set.union(*keys))
         assert 'batch' not in keys
 
@@ -42,7 +42,7 @@ class Batch(Data):
         cumsum = {key: 0 for key in keys}
         batch.batch = []
         for i, data in enumerate(data_list):
-            for key in data.keys:
+            for key in data.keys():
                 item = data[key]
                 if torch.is_tensor(item) and item.dtype != torch.bool:
                     item = item + cumsum[key]
@@ -66,7 +66,7 @@ class Batch(Data):
         if num_nodes is None:
             batch.batch = None
 
-        for key in batch.keys:
+        for key in batch.keys():
             item = batch[key][0]
             if torch.is_tensor(item):
                 batch[key] = torch.cat(batch[key],